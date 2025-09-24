79424d75c069da955119e3f7101080f3bc004e37
Nurul Aziz <sohelcse233151@gmail.com>
2025-09-24 02:34:00 -0700

    Add files via upload

---
+++ Fan/index.js
@@ -0,0 +1,21 @@
+let a = document.getElementById("img");
+
+function myfanon(){
+    a.style.animation = "spin 3s linear infinite";
+}
+
+function myfanoff(){
+    a.style.animation = "none";
+}
+
+function myfan2(){
+    a.style.animation = "spin 1s linear infinite";
+}
+
+function myfan3(){
+    a.style.animation = "spin 0.5s linear infinite";
+}
+
+function myfan4(){
+    a.style.animation = "spin 0.1s linear infinite";
+}
